6ece56de453f3c277a027aa730cbdb275d398366
ahmd-fariz <ahmadfarizmaulana4@gmail.com>
2024-07-18 14:32:47 +0700

    update absen

--- controller/AbsenController.js
+++ controller/AbsenController.js
@@ -1,6 +1,20 @@
 import Absen from "../models/Absen.js";
+import UserModel from "../models/UserModel.js";
 
-export const GetAbsens = async (req, res) => {};
+export const GetAbsens = async (req, res) => {
+  try {
+    const response = await Absen.findAll({
+      include: {
+        model: UserModel,
+        attributes: ["name","role"],
+      },
+      attributes: ["userId", "tanggal", "waktu_datang", "keterangan"],
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 export const createAbsen = async (req, res) => {
   const { userId } = req.body;
@@ -8,7 +22,7 @@ export const createAbsen = async (req, res) => {
   console.log("Request received to create absen for userId:", userId);
   const today = new Date();
   const date = today.toISOString().split("T")[0]; // Mendapatkan tanggal hari ini dalam format YYYY-MM-DD
-  const waktu_datang = today.toLocaleTimeString();
+  const waktu_datang = today.toLocaleTimeString(); // Mendapatkan Waktu hari ini dalam format am.pm
   try {
     const absen = await Absen.create({
       userId,
