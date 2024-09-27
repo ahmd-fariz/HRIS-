7dc983091281bdfb037db6e533b9729336473ed3
frashsym <rfqfrashsym@gmail.com>
2024-09-27 13:33:30 +0700

    ganti maning

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("hairis", "root", "HrisGMT@1", {
+const db = new Sequelize("u610515881_hris", "u610515881_hris", "HrisGMT@1", {
   host: "153.92.15.8",
   port: "3306",
   dialect: "mysql",
