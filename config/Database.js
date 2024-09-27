bb34a1c64cee38f7877469da2a52f4e5f0321a20
frashsym <rfqfrashsym@gmail.com>
2024-09-27 11:37:26 +0700

    pake pw

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("hairis", "root", "", {
+const db = new Sequelize("hairis", "root", "HrisGMT@1", {
   host: "153.92.15.8",
   port: "3306",
   dialect: "mysql",
